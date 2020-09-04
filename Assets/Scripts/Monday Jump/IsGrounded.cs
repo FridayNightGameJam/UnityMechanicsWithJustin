1222a25fc8b78b90685132607dd65a79f1a0091b
Justin MacArthur <macarthurjustin@gmail.com>
2020-09-03 21:46:38 -0600

    updating landing and falling

--- Assets/Scripts/Monday Jump/IsGrounded.cs
+++ Assets/Scripts/Monday Jump/IsGrounded.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class IsGrounded : MonoBehaviour
 {
+    public enum ContactType
+    {
+        Landing,
+        Falling
+    }
+
     [Header("Components")]
     [SerializeField] private BasicJump BasicJump;
+    [SerializeField] private ContactType contactType;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (contactType == ContactType.Falling) return;
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
         {
             BasicJump.SetGrounding(true);
@@ -17,6 +26,8 @@ public class IsGrounded : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (contactType == ContactType.Landing) return;
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
         {
             BasicJump.SetGrounding(false);
